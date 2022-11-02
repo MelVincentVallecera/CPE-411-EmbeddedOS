88dd09b32eea83b4a9b255c8390fc72aeba317f5
Mel Vincent Vallecera <92211965+MelVincentVallecera@users.noreply.github.com>
2022-11-02 18:21:55 +0800

    Add files via upload

---
+++ first_in_first_out.c
@@ -0,0 +1,44 @@
+// First In First Out Scheduling
+// Mel Vincent Vallecera BSCPE-4B
+
+int bt[] = {10, 5, 8, 3, 1, 2}; // Burst time inputs
+int p[] = {7, 6, 5, 4, 3, 2}; // Processes pin inputs
+int size = 6;
+float AWT, wt;
+
+void setup() {
+  // LEDs Pin configuration
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  for ( int i = 0; i < size; i++) {
+    Arrive(p[i], bt[i]); // Task arrives to queue 
+    
+  	if (i < size - 1) {
+      Wt(bt[i]); // Waiting time for task
+  	}
+  }
+  
+  Serial.println("Average Waiting Time : ");
+  Serial.println(AWT/size); // Prints the average waiting time during process
+  Serial.println("End of the Process. ");
+  delay(10000);
+}
+
+void Arrive(int task, int burstTime) {
+  digitalWrite(task, HIGH);
+  delay(burstTime * 1000); // LEDs light up to burst time
+  digitalWrite(task, LOW);
+}
+
+void Wt(int waitingTime) {
+  wt = wt + waitingTime; 
+  AWT = AWT + wt;
+}
