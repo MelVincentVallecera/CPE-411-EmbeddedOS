a3e9f137dce09eef5851317110a2e3ef77806abe
Mel Vincent Vallecera <92211965+MelVincentVallecera@users.noreply.github.com>
2022-11-04 23:09:37 +0800

    Add files via upload

---
+++ round_robin.c
@@ -0,0 +1,64 @@
+// Round Robin Scheduling
+// Mel Vincent Vallecera BSCPE - 4B
+
+int bt[6] = {6, 9, 1, 8, 5, 3}; // Burst time inputs
+int p[6] = {7, 6, 5, 4, 3, 2}; // Processes pin inputs
+int temp[6] = {0, 0, 0, 0, 0, 0};
+int size = 6, time_quantum = 2, count = 20, t = 0;
+float AWT, ft = 0;
+// Arrives at time 0
+
+void setup() {
+  // LEDs Pin configuration
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+}
+  
+void loop() {
+  while (count != 0) {
+    for (int i = 0; i < size; i++) {
+      if (bt[i] != 0) {
+        if (t < size) {
+          temp[t] = bt[i]; // a copy of burst time
+          t++;
+        }
+        if (bt[i] < time_quantum) { // if remaining burst time is less than time quantum
+          Arrive(p[i], time_quantum - 1);
+          ft = ft + 1;
+          Wt(ft - temp[i]); // Finish time - burst time
+          bt[i] = 0;
+        } else if (bt[i] == time_quantum) { // if remaining burst time is equal to time quantum
+          Arrive(p[i], time_quantum);
+          ft = ft + 2;
+          Wt(ft - temp[i]); // Finish time - burst time
+          bt[i] = 0;
+        } else if (bt[i] > time_quantum) { // if remaining burst time is greater than time quantum
+          bt[i] = bt[i] - time_quantum;
+          Arrive(p[i], time_quantum);
+          ft = ft + 2; 
+        }
+      } else {
+        count--; // terminator
+      }
+    }
+  }
+  Serial.println("Average Waiting Time : ");
+  Serial.println(AWT/size); // Prints Average Waiting Time
+  Serial.println("End of process.");
+  delay(10000);
+}
+
+void Arrive(int task, int timeQuantum) {
+  digitalWrite(task, HIGH);
+  delay(timeQuantum * 1000); // Lights up to time quantum
+  digitalWrite(task, LOW);
+}
+
+void Wt(int waitingTime) { // Summation of waiting time
+  AWT = AWT + waitingTime;
+}
